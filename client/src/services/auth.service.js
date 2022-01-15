a043f878571f58007555afbe4afe88dbd7d56e2d
Muhammad Munawwar Adam <ma04289@st.habib.edu.pk>
2022-01-15 23:23:42 +0500

    Fix Network Error

--- client/src/services/auth.service.js
+++ client/src/services/auth.service.js
@@ -1,26 +1,21 @@
 import axios from "axios";
-const API_URL = "http://localhost:5000/auth/";
-class AuthService {
-    login(user) {
-    return axios
-      .post(API_URL + "signin", user
-      )
-      .then(response => {
-        if (response.data.accessToken) {
-          localStorage.setItem("user", JSON.stringify(response.data));
-        }
 
-        return response.data;
-       
-      });
+class AuthService {
+  login(user) {
+    return axios.post("/auth/signin", user).then((response) => {
+      if (response.data.accessToken) {
+        localStorage.setItem("user", JSON.stringify(response.data));
+      }
+
+      return response.data;
+    });
   }
 
   logout() {
     localStorage.removeItem("user");
   }
-   getCurrentUser() {
-    return JSON.parse(localStorage.getItem('user'));;
+  getCurrentUser() {
+    return JSON.parse(localStorage.getItem("user"));
   }
 }
 export default new AuthService();
-
--- client/src/services/student.service.js
+++ client/src/services/student.service.js
@@ -1,23 +1,22 @@
 import axios from "axios";
 import authHeader from "./auth-header.js";
-const API_URL = "http://localhost:5000/student/";
 
 class StudentService {
   registerStudent(student) {
-    return axios.post(API_URL, student).then((response) => {
+    return axios.post("/student", student).then((response) => {
       return response.data;
     });
   }
 
   getAllStudents() {
-    return axios.get(API_URL, { headers: authHeader() });
+    return axios.get("/student", { headers: authHeader() });
   }
 
   deleteStudent(id) {
-    return axios.delete(API_URL + `${id}`, { headers: authHeader() });
+    return axios.delete(`/student/${id}`, { headers: authHeader() });
   }
   getStudent(id) {
-    return axios.get("http://localhost:5000/student/" + id);
+    return axios.get(`/student/${id}`);
   }
 }
 export default new StudentService();
